23fa51a2c743676c15af5c99a9d8cabbe50c4a2c
khainan <muhammadrachim.k@gmail.com>
2019-10-23 23:03:11 +0700

    remove all html created by CRA and get data from poke-api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/type/7/")
+      .then(res => {
+        console.log(res.data)
+      })
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
