38d008f7a57a6867679be657f5feaeba8b97c755
khainan <muhammadrachim.k@gmail.com>
2019-10-24 03:07:17 +0700

    create function set data pokemon, create function show list pokemon

--- src/App.js
+++ src/App.js
@@ -8,25 +8,56 @@ function App() {
 
   const [activeFilter, setActiveFilter] = useState("fire")
   const [listType, setListType] = useState([])
+  const [pokemon, setPokemon] = useState([])
 
-  const getDataPokemon = () => {
-    axios.get("https://pokeapi.co/api/v2/pokemon")
+  const getDataPokemon = async () => {
+    await axios.get("https://pokeapi.co/api/v2/pokemon")
       .then(res => {
-        console.log(res.data)
-      })
+        res.data.results.map(data => setDataPokemon(data.name, data.url))
+    })
   }
 
-  const getDataType = () => {
-    axios.get("https://pokeapi.co/api/v2/type")
+  const getDataType = async () => {
+    await axios.get("https://pokeapi.co/api/v2/type")
       .then(res => {
         console.log(res.data)
+        setListType(res.data.results)
+    })
+  }
+
+  const setDataPokemon = async (name, url) => {
+    let id
+    let imageUrl
+    let type = []
+    let convertedId
+
+    await axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .then(res => {
+        id = res.data.id
+        convertedId = id > 99 ? id : id > 9 ? '0'+id : '00' + id
+        imageUrl = `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${convertedId}.png`
+        res.data.types.map(val => type.push(val.type.name))
       })
+    const newObject = {
+      name: name,
+      url: url,
+      image: imageUrl,
+      id:id,
+      type:type
+    }
+
+    pokemon.push(newObject)
+    let newArray = [...pokemon]
+
+    setPokemon(newArray)
   }
 
   useEffect(() => {
     getDataPokemon()
     getDataType()
   },[])
+
+  console.log(pokemon)
 
   return (
     <div className="App">
@@ -42,6 +73,13 @@ function App() {
           <button className="button-filter flying-color">flying</button>
           <button className="button-filter electric-color">electric</button> */}
 
+        </div>
+        <div className="wrapper-list-pokemon">
+          { pokemon && pokemon.map(data =>
+            <div className="card">{data.name}
+              <img src={data.image} />
+            </div>
+          )}
         </div>
       </div>
     </div>
