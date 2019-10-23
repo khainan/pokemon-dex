9b0e6469a55f5ddc327776f83bf7ca08fd9f5cba
khainan <muhammadrachim.k@gmail.com>
2019-10-24 01:17:20 +0700

    get data type pokemon, hide button filter

--- src/App.js
+++ src/App.js
@@ -7,33 +7,41 @@ import PokeBallIcon from './images/poke-ball-icon.png';
 function App() {
 
   const [activeFilter, setActiveFilter] = useState("fire")
+  const [listType, setListType] = useState([])
 
-  //flying 3
-  //fire 10
-  //water 11
-  //grass 12
-  //electric 13
-
-  useEffect(() => {
+  const getDataPokemon = () => {
     axios.get("https://pokeapi.co/api/v2/pokemon")
       .then(res => {
         console.log(res.data)
       })
-  })
+  }
+
+  const getDataType = () => {
+    axios.get("https://pokeapi.co/api/v2/type")
+      .then(res => {
+        console.log(res.data)
+      })
+  }
+
+  useEffect(() => {
+    getDataPokemon()
+    getDataType()
+  },[])
 
   return (
     <div className="App">
       <Header />
       <div className="main-body">
         <div className="filter-button-wrapper">
-          <button className="button-filter fire-color">
+          {/* <button className="button-filter fire-color">
               Fire
               <img className="pokeball-icon" src={PokeBallIcon} />
           </button>
           <button className="button-filter water-color">water</button>
           <button className="button-filter grass-color">grass</button>
           <button className="button-filter flying-color">flying</button>
-          <button className="button-filter electric-color">electric</button>
+          <button className="button-filter electric-color">electric</button> */}
+
         </div>
       </div>
     </div>
